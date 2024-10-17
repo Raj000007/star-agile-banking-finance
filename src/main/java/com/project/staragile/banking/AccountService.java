7cd3f08dbb592d220c6e6ac9a4617beb5043a336
Rajesh B T <103110767+Raj000007@users.noreply.github.com>
2024-10-18 02:30:28 +0530

    Update AccountService.java

--- src/main/java/com/project/staragile/banking/AccountService.java
+++ src/main/java/com/project/staragile/banking/AccountService.java
@@ -3,28 +3,38 @@ package com.project.staragile.banking;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class AccountService {
-	
-	@Autowired
-	AccountRepository accountRepository;
-	
-	public Account createAccount() {
-		Account account = new Account(1010101010,"Shubham","Saving Account",20000.0);
-		return accountRepository.save(account);
-	}
 
-	public Account registerAccount(Account account) {
-		return accountRepository.save(account);
-	}
+    @Autowired
+    private AccountRepository accountRepository;
 
-	public Account getAccountDetails(int accountId) {
-		return accountRepository.findById(accountId).get();
-	}
-	
-	public Account registerDummyAccount() {
-		Account account = new Account(1010101010,"Shubham","Saving Account",20000.0);
-		return account;
-	}
+    // Create a new hardcoded account
+    public Account createAccount() {
+        Account account = new Account(1010101010, "Shubham", "Saving Account", 20000.0);
+        return accountRepository.save(account);
+    }
 
+    // Register a new account provided by the request
+    public Account registerAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+    // Retrieve account details based on account ID
+    public Account getAccountDetails(int accountId) {
+        Optional<Account> account = accountRepository.findById(accountId);
+        if (account.isPresent()) {
+            return account.get();
+        } else {
+            // Handle case where account is not found
+            throw new RuntimeException("Account with ID " + accountId + " not found.");
+        }
+    }
+
+    // Register a dummy account (for testing purposes)
+    public Account registerDummyAccount() {
+        return new Account(1010101010, "Shubham", "Saving Account", 20000.0);
+    }
 }
